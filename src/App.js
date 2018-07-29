94830fccea82ca27007dd29fc97848ccc15afe99
fahad-zakir <fahadzakir81@gmail.com>
2018-07-29 00:02:38 -0400

    added props on Form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 import Form from"./components/Form";
 
 class App extends Component {
+  //this is for the Api call getRecipe
+  //we will pass this data of getRecipe below using this.getRecipe
+  //this on line 19 refers to the app component and .getRecipe is for that data
+  getRecipe () {
+    console.log("Working!!!");
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form />
+        <Form getRecipe={this.getRecipe}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,18 @@
 import React from 'react';
-
-const Form = () => {
-  <form>
+// we have to pass in an argument here in this stateless component props
+// props referes to getRecipe from App.js and whatever gets passed in to that argument gets stored here in this argument
+// this props is an object and to access the property, you type in props.(name of property)
+// onSubmit is an attribute
+const Form = props => (
+  <form onSubmit={props.getRecipe}>
     <input type="text"/>
     <button>Search</button>
   </form>
-};
+);
 
 export default Form;
 
-//stateless component
+// stateless component
+// can't use this.props here to access .getRecipe (from app.js) since this is a stateless component
+// although (this.props) is used for passing data between components
+// 
